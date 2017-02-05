1bb5100a706f6462fcebeac9f3fdf3bee992be3d
andyliwr <andyliwr@outlook.com>
2017-02-05 22:17:19 +0800

    使得可以爬虫可以爬取爱下载小说网的小说

--- nodeServer/server/networkReptile/tools/myAppTools.js
+++ nodeServer/server/networkReptile/tools/myAppTools.js
@@ -89,7 +89,7 @@ var getElementArray = function(jsonArray, element){
 
 //去掉第和章之间的非数字
 var removeNaN = function(str){
-  var standardNum = ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '千', '百', '万', '亿'];
+  var standardNum = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '千', '百', '万', '亿'];
   if(typeof str == 'string'){
     var strArray = str.split("");
     for(var i=0; i<strArray.length; i++){
